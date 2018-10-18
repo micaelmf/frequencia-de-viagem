b0e8579f1f9e8a0a1ea71380d827351a3f767281
Micael Ferreira <micaelmf@gmail.com>
2018-10-18 13:00:59 -0300

    esqueleto do controlador

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+class IndexController
+{
+	public function criar()
+	{
+		echo 'Criar viagem';
+	}
+	
+	public function gerenciar()
+	{
+		echo 'Gerenciar viagem';
+	}
+	
+	public function _404()
+	{
+		echo 'erro 404';
+	}
+}
